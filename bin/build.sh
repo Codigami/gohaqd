a55ca604d76339d61deaee35ca62209c460ea070
deepak365 <deepak365365@gmail.com>
2022-04-05 17:11:06 +0530

    build image

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,8 @@
-echo "not required"
+#!/usr/bin/env bash
+
+# Build the code, run the tests and
+# create the "production" docker image - not necessarily in that order
+#
+# IMPORTANT: The docker image must be named <repo_name>:latest
+
+docker build -t gohaqd .
